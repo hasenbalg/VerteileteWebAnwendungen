e0d7d2ceccb930c3ac897667431ce4f2ee0e8c21
Unknown <philipp.zuelicke@gmx.de>
2017-06-08 11:38:33 +0200

    Oberfläche gestaltet

--- VWW_Project/VWW_Project/Controllers/HomeController.cs
+++ VWW_Project/VWW_Project/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VWW_Project.Models;
 
 namespace VWW_Project.Controllers
 {
@@ -23,6 +24,24 @@ namespace VWW_Project.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Calender()
+        {
+            ViewBag.Message = "Calender";
+            ViewBag.Days = new Date().getAllDatesOfYear(DateTime.Today.Year);
+            return View();
+        }
+        public ActionResult Schedule()
+        {
+            ViewBag.Message = "Calender";
+
+            return View();
+        }
+        public ActionResult OtherUser()
+        {
+            ViewBag.Message = "Calender";
 
             return View();
         }
---
+++ VWW_Project/VWW_Project/Models/Date.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VWW_Project.Models
+{
+    public class Date
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public DayOfWeek WeekDay { get; set; }
+
+        public Date[][] getAllDatesOfYear(int y)
+        {
+            Date[][] dates = new Date[12][];
+            for (int c = 0; c < 12; c++)
+            {
+                dates[c] = new Date[DateTime.DaysInMonth(y, c +1)];
+                for (int i = 0; i < dates[c].Length; i++)
+                {
+                    Date d = new Date();
+                    d.Day = i + 1;
+                    d.Month = c + 1;
+                    d.Year = y;
+                    d.WeekDay = new DateTime(y, c + 1, i + 1).DayOfWeek;
+                    dates[c][i] = d;
+                }
+            }
+            return dates;
+        }
+    }
+
+   
+}
