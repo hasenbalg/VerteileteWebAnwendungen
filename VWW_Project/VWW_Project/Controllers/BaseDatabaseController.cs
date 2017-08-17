b3acc18486969dd0284cfc15fbe43236beb8180f
Pascal-W <p.wegner1992@gmx.de>
2017-08-17 19:24:50 +0200

    DAL implemented; BaseDatabaseController implemented; Model designer reset;

---
+++ VWW_Project/VWW_Project/Controllers/BaseDatabaseController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VWW_Project.Controllers
+{
+    public abstract class BaseDatabaseController : Controller
+    {
+        //private DbModelContainer db;
+        //protected DbModelContainer Db
+        //{
+        //    get
+        //    {
+        //        if (this.db == null)
+        //        {
+        //            this.db = new DbModelContainer();
+        //        }
+        //        return this.db;
+        //    }
+        //}
+
+        //public BaseDatabaseController() { }
+        //public BaseDatabaseController(DbModelContainer db)
+        //    : this()
+        //{
+        //    this.db = db;
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        this.db.Dispose();
+        //        this.db = null;
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
